5111f5ec32318f82c9fe1e85a7b82c665600031a
daniel-j <daniel.j.hede@gmail.com>
2016-08-19 10:57:53 +0200

    Move non-linear items to the end of the book. Not all eReaders hide them, so this is an easy workaround

--- src/templates.js
+++ src/templates.js
@@ -77,6 +77,19 @@ export function createChapter (ch, html, callback) {
   })
 }
 
+// some eReaders doesn't understand linear=no, so sort those items to the end of the spine/book.
+function sortItemsLinear (a, b) {
+  let linearA = a.attrs.linear !== 'no'
+  let linearB = b.attrs.linear !== 'no'
+  if (linearA === linearB) {
+    return 0
+  } else if (linearA) {
+    return -1
+  } else {
+    return 1
+  }
+}
+
 export function createOpf (ffc) {
   let remotes = []
   ffc.remoteResources.forEach((r, url) => {
@@ -128,7 +141,7 @@ export function createOpf (ffc) {
         m('itemref', {idref: 'nav', linear: ffc.storyInfo.chapters.length <= 1 ? 'no' : undefined})
       ].concat(ffc.storyInfo.chapters.map((ch, num) =>
         m('itemref', {idref: 'chapter_' + zeroFill(3, num + 1)})
-      ))),
+      )).sort(sortItemsLinear)),
 
       m('guide', [
         m('reference', {type: 'cover', title: 'Cover', href: 'Text/cover.xhtml'}),
