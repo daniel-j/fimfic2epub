dfd6e9897e31a7d02b1b1672ecb9a4e6f0e2694a
daniel-j <daniel.j.hede@gmail.com>
2018-03-12 22:07:11 +0100

    update styles

--- src/styles.js
+++ src/styles.js
@@ -1,10 +1,10 @@
 
-let styleCss, coverstyleCss, titlestyleCss
-
-styleCss = require('./style/style')
-coverstyleCss = require('./style/coverstyle')
-titlestyleCss = require('./style/titlestyle')
-let paragraphsCss = {
+const styleCss = require('./style/style')
+const coverstyleCss = require('./style/coverstyle')
+const titlestyleCss = require('./style/titlestyle')
+const navstyleCss = require('./style/navstyle')
+const iconsCss = require('./style/icons')
+const paragraphsCss = {
   spaced: require('./style/paragraphs-spaced'),
   indented: require('./style/paragraphs-indented'),
   indentedall: require('./style/paragraphs-indentedall')
@@ -12,4 +12,4 @@ let paragraphsCss = {
 
 paragraphsCss.both = paragraphsCss.indented + '\n' + paragraphsCss.spaced
 
-export { styleCss, coverstyleCss, titlestyleCss, paragraphsCss }
+export { styleCss, coverstyleCss, titlestyleCss, navstyleCss, iconsCss, paragraphsCss }
